e9c48383e09b5808306eb1ec0f5c26bc97383aa5
HIMANSHU R AGARWAL <himanshu59cool@gmail.com>
2020-09-22 13:47:00 +0530

    angular files

---
+++ src/app/user.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <!-- <input type="text" (input)="onUserInput($event)" [value]="name"> -->
+    <input type="text" [(ngModel)]="name">
+    <p>Hello Bhidu,{{ name }}!</p>
+    <!--<p>I'm the user component</p> -->
+  `
+})
+export class UserComponent {
+  name = 'Himanshu';
+
+  onUserInput(event) {
+    this.name = event.target.value;
+  }
+}
